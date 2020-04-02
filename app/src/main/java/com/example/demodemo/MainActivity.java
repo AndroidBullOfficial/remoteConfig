df80a662bc6b3b6c8c0e2952fbf79494e5c3e359
MuhammadArslan786 <muhammad.arslan1080@gmail.com>
2020-04-02 05:52:01 +0500

    final project

--- app/src/main/java/com/example/demodemo/MainActivity.java
+++ app/src/main/java/com/example/demodemo/MainActivity.java
@@ -93,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, "Fetch failed",
                                     Toast.LENGTH_SHORT).show();
                         }
+                        //welcome
                         displayWelcomeMessage();
                     }
                 });
